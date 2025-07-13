cedab07672787ce0f5f70a31133ed76236e66dfd
Sondos Ahmed <sondosahmed@MacPro-Sondos.local>
2025-07-13 13:50:42 +0300

    update

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -12,3 +12,10 @@ class DatabaseConnection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.connection:
             self.connection.close()
+
+if __name__ == "__main__":
+    with DatabaseConnection('users.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetchall()
+        print(results)
