869cce675c1e168518c73bbdfb0b97253843128c
Sondos Ahmed <sondossfwat@gmail.com>
2025-07-26 14:32:27 +0300

    week5

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,10 +1,12 @@
 from rest_framework import permissions,BasePermission
+from rest_framework .decorators import action
 
 class IsParticipantofConversation(BasePermission):
     """
     Custom permission to check if the user is a participant of the conversation.
     """
-    def has_permission(self, request, view):
+    @action(detail=True, methods=['post','put','delete'])
+    def has_permission(self, request, view):    
         conversation_id=view.kwargs.get('pk')
         if not conversation_id:
             return False
